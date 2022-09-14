d2524aa5d6fb2c275d24bc833e63fc1be61881e7
Miles Johnson <milesj@users.noreply.github.com>
2022-09-13 20:44:13 -0700

    Upgrade to moon v0.14! (#26)
    * Update deps.
    
    * Update moon.
    
    * Add action.

--- apps/vue-vite-app/vite.config.ts
+++ apps/vue-vite-app/vite.config.ts
@@ -7,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	// @ts-expect-error Mismatched types
 	plugins: [vue(), vueJsx()],
 	resolve: {
 		alias: {
