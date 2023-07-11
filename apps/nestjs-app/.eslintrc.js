11f76f3976125313c842b019a0696788812bb0fd
Jad31 <jad.chahed@epitech.eu>
2023-07-11 13:31:19 +0200

    fix: typescript version for nestjs

--- apps/nestjs-app/.eslintrc.js
+++ apps/nestjs-app/.eslintrc.js
@@ -5,11 +5,6 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
     node: true,
--- apps/nestjs-app/test/app.e2e-spec.ts
+++ apps/nestjs-app/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import supertest from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -16,7 +16,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return supertest(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
