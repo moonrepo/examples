10b9d9cb7cbadf7c339bf4dbd2619f8191527f8d
Jad31 <jad.chahed@epitech.eu>
2023-07-19 15:04:08 +0200

    fix: prettier adn eslint configs

--- apps/nestjs-app/.eslintrc.js
+++ apps/nestjs-app/.eslintrc.js
@@ -1,16 +1,4 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
